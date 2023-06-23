60f2ebd07abf3ee2e1db56eec47e386ff9122e2b
metinsimsek <msmetinsimsek@gmail.com>
2023-06-23 13:15:33 +0300

    güncelleme

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import datetime as dt
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+print("Hello Word!")
+
+def say_hello(isim):
+    """
+    isme selam verip o anki saati söyleyen fonksiyon
+    :param isim: str
+    :return:
+    """
+    print(f"Merhaba, {isim}")
+    print(f"Şu anki saat{dt.datetime.now()}")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+say_hello("ali")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
