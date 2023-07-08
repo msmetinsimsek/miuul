5ecc56d4c54871062af2e8f8e3a0ecc3e3cde115
metinsimsek <msmetinsimsek@gmail.com>
2023-07-08 16:02:56 +0300

    sonudna yükeldim

--- scripts/tRNAscan_stats.py
+++ scripts/tRNAscan_stats.py
@@ -8,8 +8,8 @@ shell(f"""tRNAscan-SE {genome} -o {tRNA} -m {stats}""")
 
 
 #yüklem ypaparken snakefile dosyasını çalıştırmak için
-#snakemake --jobs 1 -c1 --use-conda --printshellcmds -n komutu ile snakefile in çalışıp çalışmadığını komntorl ettim
-#snakemake --jobs 1 -c1 --use-conda --printshellcmds  komutu ile dosyayı çalıştırdım ve hata aldım
+# snakemake --jobs 1 -c1 --use-conda --printshellcmds -n komutu ile snakefile in çalışıp çalışmadığını komntorl ettim
+# snakemake --jobs 1 -c1 --use-conda --printshellcmds  komutu ile dosyayı çalıştırdım ve hata aldım
 # outpıt a tRNA_scan_result.txt dosyası ekledim ve IncompleteFilesException:
 # The files below seem to be incomplete. If you are sure that certain files are not incomplete, mark them as complete with
 #
@@ -18,4 +18,5 @@ shell(f"""tRNAscan-SE {genome} -o {tRNA} -m {stats}""")
 # To re-generate the files rerun your command with the --rerun-incomplete flag.
 # Incomplete files:
 # output/tRNA_scan_result.txt ne hatası aldım
-# bu hatayı çözmek için önerdiği gibi snakemake --rerun-incomplete işlemin yapınca outputları aldım
+# hatayı çözmek için önerdiği gibi snakemake --rerun-incomplete işlemin yapınca outputları aldım
+
